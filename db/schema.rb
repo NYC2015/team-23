bc02d3d44a192f85969feb7e8578cd21634f366f
Marcell Purham <marcellpurham@gmail.com>
2015-10-17 00:47:04 -0500

    fixed and updated profile pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017022411) do
+ActiveRecord::Schema.define(version: 20151017030114) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "full_name"
+    t.string   "username"
+    t.string   "gender"
+    t.string   "age"
+    t.string   "phone_number"
+    t.boolean  "is_anonymous"
+    t.text     "about"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
